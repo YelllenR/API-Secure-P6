ffc474e6de1996daaa23903d62c155256d29a2ba
YelllenR <aurelie@rosedarbon.com>
2022-09-22 17:16:44 +0200

    Adding comments
    Adding comments to the files

--- backend/controller/likes-dislikes.js
+++ backend/controller/likes-dislikes.js
@@ -1,11 +1,37 @@
+// Importing product model file for use
 const Sauce = require('../models/sauce');
 
+
+/** Objet for likes in order to use for the verifications of user's reactions
+ * Includes: like, dislike, neutral according to the specifications
+ */
 const likeObject = {
     like: 1,
     dislike: -1,
-    neutral: 0
+    neutral: 0,
+    dislikeNotNegative: 1
 }
 
+/** Exporting userLike for use in sauce route
+ * With the parameters of request(for the request), response(the answer after the requested operations) and next
+ * @param {request} Request param
+ * @param {response} Response sent after execution 
+ * @param {next} next 
+ * 
+ * 1. The method findOne with the the property of the ObjetId in mongoose in order to find the product with the id requested. 
+ * 
+ * 2. Executes the Promise, the first one checks the operation/reaction of the user
+ *     i. A switch case in executed to check the conditions. The check is done based on the request body of like
+ * 
+ *     ii. Several cases based on the objet likeObjet
+ * 
+ *     iii. If one of the cases is true, it then calls the method updateOne and execute the increment property and the addToSet to 
+ *          either add or remove information according to the reaction
+ * 
+ *  *** the addToSet property is used to prevent dupplicates in the array usersLiked and usersDisiked
+ * 
+ * 3. Catch potential errors
+ */
 
 exports.userLike = (request, response, next) => {
 
@@ -18,7 +44,7 @@ exports.userLike = (request, response, next) => {
                 case likeObject.like:
                     Sauce.updateOne({ _id: request.params.id },
                         {
-                            $inc: { likes: 1 },
+                            $inc: { likes: likeObject.like },
                             $addToSet: { usersLiked: request.body.userId }
                         })
                         .then(() => response.status(200).json({ message: "ok like" }))
@@ -29,7 +55,7 @@ exports.userLike = (request, response, next) => {
                 case likeObject.dislike:
                     Sauce.updateOne({ _id: request.params.id },
                         {
-                            $inc: { dislikes: 1 },
+                            $inc: { dislikes: likeObject.dislikeNotNegative },
                             $addToSet: { usersDisliked: request.body.userId }
                         })
                         .then(() => response.status(200).json({ message: "ok dislike" }))
@@ -43,38 +69,27 @@ exports.userLike = (request, response, next) => {
 
                         Sauce.updateOne({ _id: request.params.id },
                             {
-                                $inc: {
-                                    likes: likeObject.neutral - 1
-                                },
-                                $pull: {
-                                    usersLiked: request.body.userId
-                                }
+                                $inc: { likes: likeObject.neutral - 1 },
+                                $pull: { usersLiked: request.body.userId }
                             })
                             .then((result) => response.status(200).json({ result }))
                             .catch(error => response.status(400).json({ message: error }))
-
-                        break;
                     }
 
                     if (result.usersDisliked.includes(request.body.userId)) {
 
                         Sauce.updateOne({ _id: request.params.id },
                             {
-                                $inc: {
-                                    dislikes: likeObject.neutral - 1
-                                },
-                                $pull: {
-                                    usersDisliked: request.body.userId,
-                                }
+                                $inc: { dislikes: likeObject.neutral - 1 },
+                                $pull: { usersDisliked: request.body.userId }
                             })
                             .then((result) => response.status(200).json({ result }))
                             .catch(error => response.status(400).json({ message: error }))
-
-                        break;
                     }
+                    break;
             }
 
         })
 
-        .catch(error => response.status(400).json({ message: error }))
+        .catch(error => response.status(500).json({ message: error }))
 }
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,8 +1,14 @@
-
+// Importing mongoose to build the product model
 const mongoose = require('mongoose');
 
-// const { buffer } = require('stream/consumers');
-
+/** Using mongoose with the class schema to build the model of the product
+ * 
+ * Type: The type of the value that it will contain (String or int)
+ * 
+ * Default: Sets a default number to zero. Starts at 0
+ * 
+ * require: The obligation to have or not the values
+ */
 const sauceSchema = mongoose.Schema({
     userId: { type: String, require: true },
     name: { type: String, require: true },
